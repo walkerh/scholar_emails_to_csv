17a9b55753b3bedd15d918659ca83e01748acc4a
Walker Hale IV <walker.hale.iv@gmail.com>
2021-11-25 15:47:54 -0600

    Black

--- src/scholaremailstocsv/__init__.py
+++ src/scholaremailstocsv/__init__.py
@@ -7,6 +7,7 @@ Here we just fetch the version from the distribution and expose as __version__.
 # Machinery to extract the version from package metadata.
 # Depends on Python 3.8+.
 from importlib.metadata import version as _version
+
 # If using importlib-metadata backport to support Python < 3.8:
 # try:
 #     from importlib.metadata import version as _version
