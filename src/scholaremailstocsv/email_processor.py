efeac40868cc367e8da5d371e8540de7a89b5ded
Walker Hale IV <walker.hale.iv@gmail.com>
2021-11-26 15:57:12 -0600

    wip

--- src/scholaremailstocsv/email_processor.py
+++ src/scholaremailstocsv/email_processor.py
@@ -12,20 +12,26 @@ from requests import get, head
 
 def process_emails(here: Path) -> None:
     assert here.is_dir(), here
-    email_paths = list(here.glob("*.eml"))
-    print("\n".join(str(p) for p in email_paths))
+    original_email_paths = list(here.glob("*.eml"))
     batches = here / "batches"
     now = datetime.now()
     timestamp = now.strftime("%Y-%m-%d_%H%M")
     new_batch = get_new_batch_dir(batches, timestamp)
-    pass  # TODO
+    do_batch(original_email_paths, new_batch)
 
 
 def get_new_batch_dir(batches: Path, timestamp: str) -> Path:
-    print(timestamp, ascii_lowercase)
     for c in ascii_lowercase:
         batch = batches / (timestamp + c)
         if not batch.exists():
             batch.mkdir(parents=True)
             return batch
-    raise RuntimeError
+    raise RuntimeError(f"too many batches with {timestamp=}")
+
+
+def do_batch(original_email_paths: list[Path], new_batch: Path) -> None:
+    new_email_paths = [
+        email_path.rename(new_batch / email_path.name)
+        for email_path in original_email_paths
+    ]
+    pass  # TODO
